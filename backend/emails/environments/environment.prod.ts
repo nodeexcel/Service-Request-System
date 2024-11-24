17e0820eaf1a46f2d1c6fef1b66157581440c988
nodeexcel <atul.etech2011@gmail.com>
2024-11-24 23:52:11 +0100

    refactored code for improved efficiency

---
+++ backend/emails/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  COMMENT_URL: "https://luna-ticket-service-production.up.railway.app/api/ticket/commentEmail",
+  USER_URL: "https://luna-backend-production.up.railway.app/api/user",
+};
