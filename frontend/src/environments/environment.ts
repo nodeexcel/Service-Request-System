09698f168d9b0214e9cab644c2db3f7b8408ba84
nodeexcel <atul.etech2011@gmail.com>
2024-12-04 16:23:42 +0100

    added fallback mechanisms for edge scenarios

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  API_URL: 'http://localhost:3000/api/user',
+  LOGIN_URL: 'http://localhost:3000/api',
+  USER_UPLOAD_URL: 'http://localhost:3002/api/user/upload',
+  GROUP_UPLOAD_URL: 'http://localhost:3003/api/group/upload',
+  COMMENT_URL: 'http://localhost:3001/api/ticket/comment',
+  TICKET_URL: 'http://localhost:3000/api/ticket',
+  GROUP_URL: 'http://localhost:3000/api/group',
+  USER_URL: 'http://localhost:3000/api/user/',
+  NOTIFICATION_URL: 'http://localhost:3000/api/notifications',
+  CLIENT_URL: 'http://localhost:3000/api/client',
+  FRONTEND_LOGIN_URL: 'http://localhost:4200/login',
+  FRONTEND_CLIENT_LOGIN_URL: 'http://localhost:4200/client-login',
+  FRONTEND_CLIENT_DASHBOARD_URL: 'http://localhost:4200/client-dashboard',
+  CLIENT_ROOM: 'http://localhost:4200/room/',
+  TICKET_HEALTH: 'http://localhost:3001/health'
+};
